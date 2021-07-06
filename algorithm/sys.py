94ff98c2af165c548a79f766a0fe0b1eb7ced7ef
daheyang <81452086+daheyang@users.noreply.github.com>
2021-07-06 21:12:41 +0900

    Update sys.py

--- algorithm/sys.py
+++ algorithm/sys.py
@@ -1,5 +1,7 @@
 import sys
 
 #문자열 입력 받기
-data = sys.stdin.readline().rstrip()
-print(data)
+data = sys.stdin.readline().rstrip() #rstrip(): 엔터가 줄 바꿈 기호로 입력되는 것을 막기 위해
+#print(data)
+
+print(f"정답은 {data}입니다.") #f-string을 통해 간단히 문자열과 정수를 함께 넣을 수 있다.
